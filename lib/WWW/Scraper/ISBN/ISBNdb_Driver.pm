ed47e4dbac91a4cecd0497d2079951cf84a2bed6
Barbie <barbie@missbarbell.co.uk>
2013-08-31 23:43:59 +0100

    enabled API v2

--- lib/WWW/Scraper/ISBN/ISBNdb_Driver.pm
+++ lib/WWW/Scraper/ISBN/ISBNdb_Driver.pm
@@ -33,9 +33,9 @@ my %editions = (
     '(electronic bk.)'  => 'eBook'
 );
 
-# in preparation for moving to API v2
 my %api_paths = (
     'v1'    => { format => 'http://isbndb.com/api/%s.xml?access_key=%s&index1=%s&results=%s&value1=%s', fields => [ qw( search_type access_key search_field results_type search_param ) ] },
+    'v2'    => { format => 'http://isbndb.com/api/v2/xml/%s/%s/%s', fields => [ qw( access_key search_type search_param ) ] }
 );
 
 #--------------------------------------------------------------------------
@@ -60,7 +60,6 @@ WWW::Scraper::ISBN::ISBNdb_Driver - Search driver for the isbndb.com online book
     print "Title: ",     $book->{title},     "\n";
     print "Author: ",    $book->{author},    "\n";
     print "Publisher: ", $book->{publisher}, "\n";
-    print "Year: ",      $book->{year},      "\n";
   }
 
 =head1 DESCRIPTION
@@ -81,21 +80,34 @@ sub search {
     $self->found(0);
     $self->book(undef);
 
-    my( $details, $details_url ) = $self->_fetch( 'books', 'isbn' => $isbn, 'details' );
-    my( $authors, $authors_url ) = $self->_fetch( 'books', 'isbn' => $isbn, 'authors' );
+    my %book;
+    if($API_VERSION eq 'v1') {
+        my( $details, $details_url ) = $self->_fetch( 'books', 'isbn' => $isbn, 'details' );
+        my( $authors, $authors_url ) = $self->_fetch( 'books', 'isbn' => $isbn, 'authors' );
 
-    return  unless $details && $self->_contains_book_data($details);
+        return  unless $details && $self->_contains_book_data($details);
 
-    my %book = (
-        book_link   => $details_url,
+        %book = (
+            book_link   => $details_url,
 
-        # deprecated
-        _source_url => $details_url
-    );
+            # deprecated
+            _source_url => $details_url
+        );
 
-    $self->_get_pubdata(\%book,$details);
-    $self->_get_details(\%book,$details);
-    $self->_get_authors(\%book,$authors);
+        $self->_get_pubdata(\%book,$details);
+        $self->_get_details(\%book,$details);
+        $self->_get_authors(\%book,$authors);
+    } else {
+        my( $details, $details_url ) = $self->_fetch( 'book', 'isbn' => $isbn, 'details' );
+
+        return  unless $details && $self->_contains_book_data($details);
+
+        %book = (
+            book_link   => $details_url,
+        );
+
+        $self->_get_details_v2(\%book,$details);
+    }
 
     $self->book(\%book);
     $self->found(1);
@@ -107,7 +119,8 @@ sub search {
 
 sub _contains_book_data {
     my( $self, $doc ) = @_;
-    return $doc->getElementsByTagName('BookData')->size > 0;
+    return $doc->getElementsByTagName('BookData')->size > 0 if($API_VERSION eq 'v1');
+    return $doc->getElementsByTagName('data')->size > 0;
 }
 
 #<ISBNdb server_time="2013-08-31T08:52:38Z">
@@ -214,6 +227,108 @@ sub _get_details {
     $book->{dewey}   = "$dewey";
 }
 
+# <isbndb>
+#   <data>
+#     <author_data>
+#       <id>schwartz_randal_l</id>
+#       <name>Schwartz, Randal L.</name>
+#     </author_data>
+#     <author_data>
+#       <id>tom_phoenix</id>
+#       <name>Tom Phoenix</name>
+#     </author_data>
+#     <author_data>
+#       <id>brian_d_foy</id>
+#       <name>brian d foy</name>
+#     </author_data>
+#     <awards_text></awards_text>
+#     <book_id>learning_perl_a04</book_id>
+#     <dewey_decimal>005</dewey_decimal>
+#     <dewey_normal>5</dewey_normal>
+#     <edition_info>Paperback; 2005-07-01</edition_info>
+#     <isbn10>1600330207</isbn10>
+#     <isbn13>9781600330209</isbn13>
+#     <language></language>
+#     <lcc_number></lcc_number>
+#     <marc_enc_level>~</marc_enc_level>
+#     <notes></notes>
+#     <physical_description_text>7.0"x9.2"x0.9"; 1.3 lb; 704 pages</physical_description_text>
+#     <publisher_id>oreilly_media</publisher_id>
+#     <publisher_name>O\'Reilly Media</publisher_name>
+#     <publisher_text>O\'Reilly Media</publisher_text>
+#     <subject_ids>computers_internet_programming_introductory_beginning_genera</subject_ids>
+#     <subject_ids>computers_internet_programming_languages_tools_general</subject_ids>
+#     <subject_ids>computers_internet_programming_general</subject_ids>
+#     <subject_ids>computers_internet_web_development_programming_general</subject_ids>
+#     <summary>"Learning Perl, better known as "the Llama book," starts the programmer on the way to mastery. Written by three prominent members of the Perl community who each have several years of experience teaching Perl around the world, this edition has been updated to account for all the recent changes to the language up to Perl 5.8. Perl is the language for people who want to get work done. It started as a tool for Unix system administrators who needed something powerful for small tasks. Since then, Perl has blossomed into a full-featured programming language used for web programming, database manipulation, XML processing, and system administration--on practically all platforms--while remaining the favorite tool for the small daily tasks it was designed for. You might start using Perl because you need it, but you\'ll continue to use it because you love it. Informed by their years of success at teaching Perl as consultants, the authors have re-engineered the Llama to better match the pace and scope appropriate for readers getting started with Perl, while retaining the detailed discussion, thorough examples, and eclectic wit for which the Llama is famous. The book includes new exercises and solutions so you can practice what you\'ve learned while it\'s still fresh in your mind. Here are just some of the topics covered: Perl variable types subroutines file operations regular expressions text processing strings and sorting process management using third party modules If you ask Perl programmers today what book they relied on most when they were learning Perl, you\'ll find that an overwhelming majority will point to the Llama. With good reason. Other books mayteach you to program in Perl, but this book will turn you into a Perl programmer.</summary>
+#     <title>Learning Perl</title>
+#     <title_latin>Learning Perl</title_latin>
+#     <title_long>Learning Perl (4th Edition)</title_long>
+#     <urls_text></urls_text>
+#   </data>
+#   <index_searched>isbn</index_searched>
+# </isbndb>
+
+sub _get_details_v2 {
+    my( $self, $book, $doc ) = @_;
+
+    my $people = $doc->findnodes('//data/author_data/name');
+    my @people;
+    for( my $i = 0; $i < $people->size; $i++ ) {
+        my $person = $people->get_node($i);
+        push @people, $person->to_literal;
+    }
+
+    my $str = join '; ', @people;
+    $str =~ s/^\s+//;
+    $str =~ s/\s+$//;
+
+    $book->{author} = $str;
+    
+    $book->{publisher} = ($doc->findnodes('//data/publisher_name'))[0]->to_literal;
+
+    my $isbn10 = $doc->findnodes('//data/isbn10')->to_literal;
+    my $isbn13 = $doc->findnodes('//data/isbn13')->to_literal;
+
+    $book->{isbn}   = $isbn13;
+    $book->{ean13}  = $isbn13;
+    $book->{isbn13} = $isbn13;
+    $book->{isbn10} = $isbn10;
+
+    my $long_title  = eval { ($doc->findnodes('//data/title_long'))[0]->to_literal };
+    my $short_title = eval { ($doc->findnodes('//data/title'))[0]->to_literal };
+    $book->{title} = $long_title || $short_title;
+
+    my $pubtext = $doc->findnodes('//data/publisher_text')->to_literal;
+    my $edition = $doc->findnodes('//data/edition_info')->to_literal;
+    my $desc    = $doc->findnodes('//data/physical_description_text')->to_literal;
+    my $dewey   = $doc->findnodes('//data/dewey_decimal')->to_literal;
+    my $summary = $doc->findnodes('//data/summary')->to_literal;
+
+    my ($binding,$date) = $edition =~ /([^;]+);(.*)/;
+    my (@size)          = $desc =~ /([\d\.]+)"x([\d\.]+)"x([\d\.]+)"/;
+    my ($weight)        = $desc =~ /([\d\.]+) lbs?/;
+    my ($pages)         = $desc =~ /(\d) pages/;
+    ($pages)            = $desc =~ /(\d+) p\./ unless($pages);
+
+    if( !$date && $pubtext =~ /(\d{4})/ ) { $date = $1 }
+    if( !$date && $edition =~ /(\d{4})/ ) { $date = $1 }
+
+    my ($height,$width,$depth) = sort {$b <=> $a} @size;
+
+    $book->{height}  = $height * 10     if($height);
+    $book->{width}   = $width  * 10     if($width);
+    $book->{depth}   = $depth  * 10     if($depth);
+    $book->{weight}  = $weight * $LB2G  if($weight);
+    $book->{pubdate} = $date    if($date);
+    $book->{binding} = $editions{$edition} || $binding || $edition;
+    $book->{pages}   = $pages;
+    $book->{dewey}   = "$dewey";
+
+    $book->{description} = $summary;
+}
+
+
 
 sub _fetch {
     my( $self, @args ) = @_;
@@ -272,6 +387,12 @@ sub _get_key {
         $ACCESS_KEY = $key;
         return $ACCESS_KEY;
     }
+}
+
+sub _api_version {
+    my $version = shift;
+    $API_VERSION = $version if($version && $api_paths{$version});
+    return $API_VERSION;
 }
 
 1;

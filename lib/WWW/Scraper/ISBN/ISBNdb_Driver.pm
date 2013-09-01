d46ba607a86b7d51ec79f386f74054a843ae5578
Barbie <barbie@missbarbell.co.uk>
2013-09-01 09:27:06 +0100

    upped version

--- lib/WWW/Scraper/ISBN/ISBNdb_Driver.pm
+++ lib/WWW/Scraper/ISBN/ISBNdb_Driver.pm
@@ -3,7 +3,7 @@ package WWW::Scraper::ISBN::ISBNdb_Driver;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 #--------------------------------------------------------------------------
 
